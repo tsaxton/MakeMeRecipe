707a012ec883cfbb2d0c70f92fc77dc2fdf9dda6
Nate Harner <nateharner01@gmail.com>
2013-05-12 16:03:32 -0500

    directory reorganization

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nathanharner/Documents/MakeMeRecipe/django_backend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
