ac58befd9cfadf62b29322f36ec4a8824072dee8
Angela Jiang <angelajiang@wireless-165-124-241-165.nuwlan.northwestern.edu>
2013-05-15 22:55:22 -0400

    search by preptime

--- pages/SearchResult.php
+++ pages/SearchResult.php
@@ -2,13 +2,22 @@
 if($_GET['ingredients']){
     $query = $_GET['ingredients'];
     $recipes = search($query);
+    $sortedByPrep = sortRecipesByPrepTime($recipes);
+    $recipes = $sortedByPrep;
+    
+    echo $recipes[0]['id'];
 
     foreach($recipes as $recipe){
 	echo "<h2><a href=\"?page=recipe&id={$recipe['id']}\">{$recipe['name']}</a></h2>";
+    
+    echo "Prep time: ";
+    echo $recipe['totalTimeInSeconds']/60 . " minutes. <br>";
 	echo "Ingredients:\n<ul>";
+    
 	foreach($recipe['ingredients'] as $ingredient){
 	    echo "<li>$ingredient</li>";
 	}
+    
 	echo "</ul>";
     }
 }
