d292743bf761007536cb6965808d3d34528c218f
Nate Harner <nateharner01@gmail.com>
2013-05-16 21:33:28 -0500

    bootstrapped searchResults page

--- pages/SearchResult.php
+++ pages/SearchResult.php
@@ -1,3 +1,4 @@
+<div class="page-header"><h2>Search Results</h2></div>
 <?php
 if($_GET){
     if ($_GET['maxTotalTimeInSeconds']){
@@ -21,18 +22,33 @@ if($_GET){
     $sortedByPrep = sortRecipesByPrepTime($recipes);
     $recipes = $sortedByPrep;
 
+    echo "<div class='bs-docs-grid'>";
     foreach($recipes as $recipe){
+	echo "
+	<div class='row show-grid'>
+	    <div class='span1'>
+	";	
 	if($recipe['image']){
-	echo "<img src=\"{$recipe['image']}\" class=\"img-rounded\" align=\"left\">";
+	echo "<img src='{$recipe['image']}' class='img-rounded' align='center'>";
 	}
-	echo "<a href=\"http://www.yummly.com/recipe/{$recipe['id']}\"><h2>{$recipe['name']}</h2>\n<p>See Recipe on Yummly</p></a>";
-	echo "<p>Prep Time: " . $recipe['totalTimeInSeconds']/60 . " minutes<br>";
-	echo "Ingredients:\n<ul>";
+	echo "
+	    </div>
+	    <div class='span11'>
+		<h2><a href=\"http://www.yummly.com/recipe/{$recipe['id']}\">{$recipe['name']}</h2>
+		<p>See Recipe on Yummly</p></a>
+		<p>Prep Time: " . $recipe['totalTimeInSeconds']/60 . " minutes</p>
+		<p>Ingredients:</p>
+		<ul>
+	";
 	foreach($recipe['ingredients'] as $ingredient){
 	    echo "<li>$ingredient</li>";
 	}
-	echo "</ul></p>";
+	echo "
+		</ul>
+	    </div>
+	</div>";
     }
+    echo "</div>";
 }
 else{
     echo "You did not specify a query.";
